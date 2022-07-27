2d9961b4be04c296d0c0922ea9edf7cd9a42fed4
root <root@itzvsi-h1f897w4.dte.demo.ibmcloud.com>
2022-07-27 02:17:34 -0500

    add jenkin-docker.sh

---
+++ jenkins-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+export INSTANA_AGENT_KEY="Y1WeVUl6klDiPi3WPtqBMv"
+
+# Build 
+echo start build
+sudo docker compose build
+
+# Deploy
+echo start up
+sudo nohup docker compose up >/dev/null 2>&1 &
+
+# Send request with time and version to Instana
+line=`cat .env |head -n 3|tail -n 1`
+version=${line: 4}
+current=`date "+%Y-%m-%d %H:%M:%S"`
+timeStamp=`date -d "$current" +%s`
+Date=`date "+%N"`
+currentTimeStamp=$((timeStamp*1000+$((10#$Date))/1000000))
+curl -X POST https://168.1.53.208/api/releases -H "Authorization: apiToken 3GJWpwdWwI7O" -H "Content-Type: application/json" -d '{"name": "'${version}'", "start": '${currentTimeStamp}', "applications": [{"name": "robot-shop-ce"}]}' -k
