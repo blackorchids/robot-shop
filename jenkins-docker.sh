52774367cdebcf60138ef72de18cb12962e53862
blackorchids <43508802+blackorchids@users.noreply.github.com>
2022-07-27 18:18:18 +0700

    Update jenkins-docker.sh

--- jenkins-docker.sh
+++ jenkins-docker.sh
@@ -8,7 +8,7 @@ sudo docker-compose build
 # Deploy
 echo start up
 #sudo nohup docker compose up >/dev/null 2>&1 &
-docker-compose up
+sudo docker-compose up
 echo "finish start up"
 
 # Send request with time and version to Instana
